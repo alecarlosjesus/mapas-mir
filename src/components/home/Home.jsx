9c8b6a2507426a4896786367042be8504a42962d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-08 15:02:58 -0300

    Titulo logo

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import logoBase from '../../img/bg_section1_first_anniversary.jpg'
 import './Home.css'
 export default function Home() {
+
+
+
+
     return (
         <div>
+
+            <h1 className="titLogo">
+                Dampio FAKE
+            </h1>
+
             <figure>
                 <img src={logoBase} className="bg_section1_first_anniversary" alt="logo do mir" />
             </figure>
